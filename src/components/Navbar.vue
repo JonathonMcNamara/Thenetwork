1541f0c46fe77d40e87ea592f3fb92a3c76b5620
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-08-19 11:32:45 -0600

    Update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,5 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
-      </div>
-    </router-link>
     <button
       class="navbar-toggler"
       type="button"
@@ -20,10 +15,10 @@
       <ul class="navbar-nav me-auto">
         <li>
           <router-link
-            :to="{ name: 'About' }"
+            :to="{ name: 'Home' }"
             class="btn text-success lighten-30 selectable text-uppercase"
           >
-            About
+            Home
           </router-link>
         </li>
       </ul>
