e29c938db0c872f5e044c73493f785cee4130f52
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-08-19 14:06:14 -0600

    updated post cards

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,17 +1,38 @@
 <template>
+.<div class="container d-flex justify-content-center">
+  <div class="row">
+    <div class="col-4" v-for="p in posts" :key="p.id">
+      <PostCard :post="p"/>
+    </div>
+  </div>
+</div>
 
 </template>
-
 <script>
-export default {
-setup(){
-  
+import { computed } from '@vue/reactivity';
+import { onMounted } from 'vue';
+import { AppState } from '../AppState.js';
+import { Post } from '../models/Post.js';
+import { postsService } from '../services/PostsService.js';
+import { logger } from '../utils/Logger.js';
+import Pop from '../utils/Pop.js';
 
-  return{}
+export default {
+
+setup() {
+    onMounted(async ()=>{
+      try {
+      await postsService.getPosts()  
+      } catch (error) {
+      logger.error('[Getting Posts]',error)
+      Pop.error(error) 
+      }
+    })
+  return {
+    posts: computed(()=> AppState.posts),
+  };
 }
 }
 </script>
-
-<style scoped lang="scss">
-
+<style>
 </style>
