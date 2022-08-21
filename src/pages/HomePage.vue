6184a0092fa4c35beeeaee28687cf385f7de5ebe
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-08-21 13:26:01 -0600

    updates

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,12 @@
 <template>
-.<div class="container">
+<div class="container">
+  <div class="row">
+    <div class=" offset-3 col-6">
+      <PostForm/>
+    </div>
+  </div>
+</div>
+<div class="container">
   <div class="row">
     <div class=" offset-3 col-6" v-for="p in posts" :key="p.id">
       <PostCard :post="p"/>
@@ -19,28 +26,34 @@ import { AppState } from '../AppState.js';
 import { postsService } from '../services/PostsService.js';
 import { logger } from '../utils/Logger.js';
 import Pop from '../utils/Pop.js';
+import PostForm from '../components/PostForm.vue';
 
 export default {
-
-setup() {
-    onMounted(async ()=>{
-      try {
-      await postsService.getPosts()  
-      } catch (error) {
-      logger.error('[Getting Posts]',error)
-      Pop.error(error) 
-      }
-    })
-  return {
-    posts: computed(()=> AppState.posts),
-    nextPage: computed(()=> AppState.nextPage),
-    previousPage: computed(()=> AppState.previousPage),
-    async changePage(url){
-      await postsService.changePage(url)
-      logger.error('[Changing Url]',error)
-    }
-  }
-}
+    setup() {
+        onMounted(async () => {
+            try {
+                await postsService.getPosts();
+            }
+            catch (error) {
+                logger.error("[Getting Posts]", error);
+                Pop.error(error);
+            }
+        });
+        return {
+            posts: computed(() => AppState.posts),
+            nextPage: computed(() => AppState.nextPage),
+            previousPage: computed(() => AppState.previousPage),
+            async changePage(url) {
+                await postsService.changePage(url);
+                logger.error("[Changing Url]", error);
+            },
+            async createPost() {
+                await postsService.createPost();
+                logger.error("[Creating Post]", error);
+            }
+        };
+    },
+    components: { PostForm }
 }
 </script>
 <style>
